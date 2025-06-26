059ce29780e40f5dd40c5a8cec235c5c8b80e4d1
Tombusselen <tom.busselen@gmail.com>
2025-06-26 11:29:49 +0200

    Add files via upload

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tbussele <tbussele@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 11:10:14 by tbussele          #+#    #+#             */
-/*   Updated: 2025/06/25 14:46:03 by tbussele         ###   ########.fr       */
+/*   Updated: 2025/06/26 11:25:52 by tbussele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ static char	*free_static(char *hold)
 {
 	if (hold != NULL)
 		free(hold);
-	//hold = NULL;
 	return (NULL);
 }
 
@@ -28,7 +27,11 @@ static char	*get_hold(char *stock, char *tmp, char *hold, int fd)
 	{
 		byte_rd = read(fd, stock, BUFFER_SIZE);
 		if (byte_rd < 0)
+		{
+			if (hold != NULL)
+				free(hold);
 			return (NULL);
+		}
 		if (byte_rd == 0)
 			break ;
 		stock[byte_rd] = '\0';
@@ -95,104 +98,9 @@ char	*get_next_line(int fd)
 		return (NULL);
 	line = get_line(hold);
 	if (line == NULL)
-		return (free_static(hold));
+		return (hold = free_static(hold));
 	tmp = new_hold(hold);
 	free(hold);
 	hold = tmp;
 	return (line);
 }
-/*#include "get_next_line.h"
-
-static char *free_static(char *ptr)
-{
-    if (ptr)
-        free(ptr);
-    return (NULL);
-}
-
-static char *get_hold(char *stock, char *hold, int fd)
-{
-    int bytes_read;
-    char *tmp;
-
-    while (!ft_strchr(hold, '\n'))
-    {
-        bytes_read = read(fd, stock, BUFFER_SIZE);
-        if (bytes_read < 0)
-            return (free_static(hold));
-        if (bytes_read == 0)
-            break;
-        stock[bytes_read] = '\0';
-        if (!hold)
-            hold = ft_strdup(stock);
-        else
-        {
-            tmp = ft_strjoin(hold, stock);
-            free(hold);
-            hold = tmp;
-        }
-        if (!hold)
-            return (NULL);
-    }
-    return (hold);
-}
-
-static char *get_line(char *hold)
-{
-    char *line;
-    size_t len;
-    char *newline_pos;
-
-    if (!hold || hold[0] == '\0')
-        return (NULL);
-    newline_pos = ft_strchr(hold, '\n');
-    if (newline_pos)
-        len = newline_pos - hold + 1;
-    else
-        len = ft_strlen(hold);
-    line = ft_substr(hold, 0, len);
-    return (line);
-}
-
-static char *new_hold(char *hold)
-{
-    char *newline_pos;
-    char *new_hold;
-
-    newline_pos = ft_strchr(hold, '\n');
-    if (!newline_pos)
-        return (NULL);
-    new_hold = ft_strdup(newline_pos + 1);
-    return (new_hold);
-}
-
-char *get_next_line(int fd)
-{
-    static char *hold;
-    char *stock;
-    char *line;
-    char *tmp;
-
-    if (fd < 0 || BUFFER_SIZE <= 0)
-        return (NULL);
-    stock = malloc(BUFFER_SIZE + 1);
-    if (!stock)
-        return (NULL);
-    stock[0] = '\0';
-    hold = get_hold(stock, hold, fd);
-    free(stock);
-    if (!hold)
-        return (NULL);
-    line = get_line(hold);
-    if (!line)
-    {
-	free(hold);
-	hold = NULL;
-	return (NULL);
-    }
-    tmp = new_hold(hold);
-    free(hold);
-    hold = tmp;
-    return (line);
-}*/
-
